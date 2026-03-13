c38a6da14a55ea0599f0252912bfa95d72107e2c
Agonymouss <didipizi@proton.me>
2026-03-13 10:47:04 +0100

    Update config.js

--- config.js
+++ config.js
@@ -23,8 +23,8 @@ module.exports = {
   nodes: [
      {
     name: "GlaceYT",
-    password: "https://discord.gg/mjS5J2K3ep",
-    host: "lava-v4.millohost.my.id",
+    password: "youshallnotpass",
+    host: "lavalink.jirayu.net",
     port: 443,
     secure: true
     }
